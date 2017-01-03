972484d590c488ca805e3e9df2be7f54d180e6f7
larvi530 <lars.vising@uadm.uu.se>
2017-01-03 14:54:22 +0100

    New assembly name

--- UUIntegrations/EtableraStudentkontoAdapter/Properties/AssemblyInfo.cs
+++ UUIntegrations/EtableraStudentkontoAdapter/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using Microsoft.BizTalk.XLANGs.BTXEngine;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EtableraStudentkontoAdapter")]
+[assembly: AssemblyTitle("IdentityAkkaAdapter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EtableraStudentkontoAdapter")]
+[assembly: AssemblyProduct("IdentityAkkaAdapter")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
