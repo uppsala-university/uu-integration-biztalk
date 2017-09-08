e7b63845fcb76c5e1a60686276a0b320b3103d80
EXCHANGE-TEST\_ferpi108 <fernando.pires@user.uu.se>
2017-09-08 18:45:34 +0200

    Added Helper class

---
+++ INT0001.Ladok2/Src/INT0001.Ladok2.Helper/Helper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.BizTalk.ExplorerOM;
+using System.Collections;
+
+namespace INT0001.Ladok2
+{
+  public class Helper
+  {
+    public static BtsCatalogExplorer bceExplorer = (BtsCatalogExplorer) null;
+    public static ReceivePort port = (ReceivePort) null;
+    public static bool state = true;
+
+    public static void UpdateState(string portname, bool enabled)
+    {
+      if (Helper.port == null)
+      {
+        Helper.bceExplorer = new BtsCatalogExplorer();
+        Helper.bceExplorer.ConnectionString = BtsConnectionHelper.MgmtDBConnectionString;
+        Helper.port = Helper.bceExplorer.ReceivePorts[portname];
+      }
+      foreach (ReceiveLocation receiveLocation in (ReadOnlyCollectionBase) Helper.port.ReceiveLocations)
+        receiveLocation.Enable = enabled;
+      Helper.bceExplorer.SaveChanges();
+      Helper.state = enabled;
+    }
+
+    public static bool State
+    {
+      get
+      {
+        return Helper.state;
+      }
+    }
+  }
+}
