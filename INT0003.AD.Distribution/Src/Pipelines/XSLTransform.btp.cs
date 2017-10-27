0a27beaf040ae6340c25f02db08064308de69507
EXCHANGE-TEST\jonam895 <jonas.amneus@uadm.uu.se>
2017-10-27 08:44:01 +0200

    Added functionality to add and remove membership i courses in AD.

---
+++ INT0003.AD.Distribution/Src/Pipelines/XSLTransform.btp.cs
@@ -0,0 +1,64 @@
+namespace INT0003.AD.Distribution.Pipelines
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.BizTalk.PipelineOM;
+    using Microsoft.BizTalk.Component;
+    using Microsoft.BizTalk.Component.Interop;
+    
+    
+    public sealed class XSLTransform : Microsoft.BizTalk.PipelineOM.SendPipeline
+    {
+        
+        private const string _strPipeline = "<?xml version=\"1.0\" encoding=\"utf-16\"?><Document xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instanc"+
+"e\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" MajorVersion=\"1\" MinorVersion=\"0\">  <Description /> "+
+" <CategoryId>8c6b051c-0ff5-4fc2-9ae5-5016cb726282</CategoryId>  <FriendlyName>Transmit</FriendlyName"+
+">  <Stages>    <Stage>      <PolicyFileStage _locAttrData=\"Name\" _locID=\"1\" Name=\"Pre-Assemble\" minO"+
+"ccurs=\"0\" maxOccurs=\"-1\" execMethod=\"All\" stageId=\"9d0e4101-4cce-4536-83fa-4a5040674ad6\" />      <Co"+
+"mponents>        <Component>          <Name>BizTalkComponents.PipelineComponents.XSLTTransform,BizTa"+
+"lkComponents.PipelineComponents.XSLTransform, Version=1.0.0.1, Culture=neutral, PublicKeyToken=47190"+
+"f56632fbc76</Name>          <ComponentName>XSLT Transformation</ComponentName>          <Description"+
+">Pipeline Component to apply BizTalk map with context as argument.</Description>          <Version>1"+
+".0.0</Version>          <Properties>            <Property Name=\"MapName\" />            <Property Nam"+
+"e=\"Parameters\" />          </Properties>          <CachedDisplayName>XSLT Transformation</CachedDisp"+
+"layName>          <CachedIsManaged>true</CachedIsManaged>        </Component>      </Components>    "+
+"</Stage>    <Stage>      <PolicyFileStage _locAttrData=\"Name\" _locID=\"2\" Name=\"Assemble\" minOccurs=\""+
+"0\" maxOccurs=\"1\" execMethod=\"All\" stageId=\"9d0e4107-4cce-4536-83fa-4a5040674ad6\" />      <Components"+
+" />    </Stage>    <Stage>      <PolicyFileStage _locAttrData=\"Name\" _locID=\"3\" Name=\"Encode\" minOcc"+
+"urs=\"0\" maxOccurs=\"-1\" execMethod=\"All\" stageId=\"9d0e4108-4cce-4536-83fa-4a5040674ad6\" />      <Comp"+
+"onents />    </Stage>  </Stages></Document>";
+        
+        private const string _versionDependentGuid = "4cb94ad4-9b87-4197-b0e3-e612c1d5c57d";
+        
+        public XSLTransform()
+        {
+            Microsoft.BizTalk.PipelineOM.Stage stage = this.AddStage(new System.Guid("9d0e4101-4cce-4536-83fa-4a5040674ad6"), Microsoft.BizTalk.PipelineOM.ExecutionMode.all);
+            IBaseComponent comp0 = Microsoft.BizTalk.PipelineOM.PipelineManager.CreateComponent("BizTalkComponents.PipelineComponents.XSLTTransform,BizTalkComponents.PipelineComponents.XSLTransform, Version=1.0.0.1, Culture=neutral, PublicKeyToken=47190f56632fbc76");;
+            if (comp0 is IPersistPropertyBag)
+            {
+                string comp0XmlProperties = "<?xml version=\"1.0\" encoding=\"utf-16\"?><PropertyBag xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-inst"+
+"ance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">  <Properties>    <Property Name=\"MapName\" />    "+
+"<Property Name=\"Parameters\" />  </Properties></PropertyBag>";
+                PropertyBag pb = PropertyBag.DeserializeFromXml(comp0XmlProperties);;
+                ((IPersistPropertyBag)(comp0)).Load(pb, 0);
+            }
+            this.AddComponent(stage, comp0);
+        }
+        
+        public override string XmlContent
+        {
+            get
+            {
+                return _strPipeline;
+            }
+        }
+        
+        public override System.Guid VersionDependentGuid
+        {
+            get
+            {
+                return new System.Guid(_versionDependentGuid);
+            }
+        }
+    }
+}
