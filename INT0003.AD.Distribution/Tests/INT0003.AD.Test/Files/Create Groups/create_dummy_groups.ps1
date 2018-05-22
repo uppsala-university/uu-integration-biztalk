bbf72544b4c31da0bbb336aa5e7cfe74e21b50ac
ministerkrister <kristofer.strom@uadm.uu.se>
2018-05-22 09:50:49 +0200

    Small change in comment

--- INT0003.AD.Distribution/Tests/INT0003.AD.Test/Files/Create Groups/create_dummy_groups.ps1
+++ INT0003.AD.Distribution/Tests/INT0003.AD.Test/Files/Create Groups/create_dummy_groups.ps1
@@ -1,4 +1,5 @@
-﻿#Creates dummy Create group messages. replaces the string 67586 in the groupname with a random guid. Change the span 1..10 to create a differnt amount of files
+﻿# Creates dummy group messages from original file dummy.original. Replaces the string 67586 in the groupname with a random guid. 
+# Change the span 1..10 to create a differnt amount of files
 $OldString  = '67586'
 1..10 | % {
     $guid = [guid]::NewGuid().ToString()
