151b550f335959b176ae17a7ebe3c7385dc4c70f
kimst498 <kimst498@UUC-BIZDEV008>
2018-08-06 15:18:33 +0200

    Adding msi and bindings.

--- INT0018.Delayed.Events/Src/Files/PROCEDURE.sql
+++ INT0018.Delayed.Events/Src/Files/PROCEDURE.sql
@@ -1,4 +1,3 @@
 CREATE PROCEDURE addDelayedEvent @body varchar(MAX), @eventTime datetime, @ack int
 AS
-INSERT INTO dbo.delayed (body,eventTime,ack) VALUES (@body, @eventTime, @ack)
-GO;
+INSERT INTO dbo.delayed_events (body,eventTime,ack) VALUES (@body, @eventTime, @ack);
--- INT0018.Delayed.Events/Src/Files/delayed_events.sql
+++ INT0018.Delayed.Events/Src/Files/delayed_events.sql
@@ -1,6 +1,6 @@
 CREATE TABLE delayed_events (
     id int IDENTITY(1,1) PRIMARY KEY,
     body varchar(MAX) NOT NULL,
-    timestamp timestamp NOT NULL,
+    eventTime datetime NOT NULL,
     ack int NOT NULL
 );
