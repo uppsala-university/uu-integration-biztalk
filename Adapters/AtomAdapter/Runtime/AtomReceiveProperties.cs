408733125e5d3d2438cf239de98a74ff85943108
ministerkrister <kristofer.strom@uadm.uu.se>
2018-08-20 10:25:52 +0200

    Code cleanup and formatting

--- Adapters/AtomAdapter/Runtime/AtomReceiveProperties.cs
+++ Adapters/AtomAdapter/Runtime/AtomReceiveProperties.cs
@@ -1,20 +1,12 @@
 
-using System;
-using System.IO;
-using System.Xml;
-using System.Net;
-using System.Collections;
-using Microsoft.BizTalk.Message.Interop;
 using Microsoft.BizTalk.Adapter.Common;
 using Shared.Components;
+using System;
+using System.Net;
+using System.Xml;
 
 namespace BizTalk.Adapter.Atom
 {
-
-   // public enum SizeUnit { Bytes, KBytes, MBytes }
-
-    // //////////////////////////////////////////////////////////////////////////////////////////////
-
     internal class AtomReceiveProperties : ConfigProperties
     {
         private string uri;
@@ -31,9 +23,10 @@ namespace BizTalk.Adapter.Atom
 
         private int feedMax = 0;
 
-        //private string clientCertificate;
-        public SecuritySettings SecuritySettings {
-            get {
+        public SecuritySettings SecuritySettings
+        {
+            get
+            {
                 return securitySettings;
             }
         }
@@ -49,17 +42,8 @@ namespace BizTalk.Adapter.Atom
 
         public string FirstFeed { get { return firstFeed; } }
         public bool FirdFirst { get { return findFirst; } }
-        /*
-        public static TimeSpan Time(string time)
-        {
-            TimeSpan span;
-            if (TimeSpan.TryParse(time, out span) == false)
-                span = new TimeSpan(0, 0, 1);
 
-            return span;
-        }
-        */
-        public AtomReceiveProperties (string uri)
+        public AtomReceiveProperties(string uri)
         {
             try
             {
@@ -70,22 +54,20 @@ namespace BizTalk.Adapter.Atom
                 this.address = String.Empty;
                 this.userName = String.Empty;
                 this.password = String.Empty;
-                //this.clientCertificate = String.Empty;
                 this.findFirst = false;
                 this.firstFeed = String.Empty;
                 this.feedMax = 10;
-
             }
             finally
             {
             }
         }
 
-        public virtual void HandlerConfiguration (XmlDocument configDOM)
+        public virtual void HandlerConfiguration(XmlDocument configDOM)
         {
         }
 
-        public virtual void LocationConfiguration (XmlDocument configDOM,bool update)
+        public virtual void LocationConfiguration(XmlDocument configDOM, bool update)
         {
             try
             {
@@ -107,9 +89,7 @@ namespace BizTalk.Adapter.Atom
 
                 XmlNode nodePollingInterval = configDOM.SelectSingleNode("Config/pollingInterval");
 
-
                 XmlNode nodeAddress = configDOM.SelectSingleNode("Config/address");
-
                 if (nodeAddress == null)
                     throw new ArgumentNullException("NodeAddress", "Atom feed address must be specified!");
 
@@ -120,14 +100,12 @@ namespace BizTalk.Adapter.Atom
                 this.pollingInterval = pollingIntervalMultiplier * pollingIntervalValue;
 
                 XmlNode nodeStateFile = configDOM.SelectSingleNode("Config/stateFile");
-
                 if (nodeStateFile == null)
                     throw new ArgumentNullException("NodeStateFile", "Path to state file must be specified!");
 
                 this.stateFile = nodeStateFile.InnerText;
 
                 XmlNode userName = configDOM.SelectSingleNode("Config/userName");
-
                 if (userName != null)
                 {
                     XmlNode password = configDOM.SelectSingleNode("Config/password");
@@ -136,13 +114,12 @@ namespace BizTalk.Adapter.Atom
                         securitySettings = new SecuritySettings();
 
                     securitySettings.Credentials = new NetworkCredential(StringIFNull(userName), StringIFNull(password));
-                
+
 
                 }
 
                 XmlNode clientCertificate = configDOM.SelectSingleNode("Config/clientCertificate");
-
-                if(clientCertificate != null)
+                if (clientCertificate != null)
                 {
                     if (securitySettings == null)
                         securitySettings = new SecuritySettings();
@@ -151,15 +128,12 @@ namespace BizTalk.Adapter.Atom
                 }
 
                 XmlNode firstFeed = configDOM.SelectSingleNode("Config/firstFeed");
-
-                if(firstFeed != null)
+                if (firstFeed != null)
                 {
                     this.firstFeed = firstFeed.InnerText;
                 }
 
-
                 XmlNode findFirst = configDOM.SelectSingleNode("Config/findFirst");
-
                 if (findFirst != null)
                 {
                     this.findFirst = Boolean.Parse(findFirst.InnerText);
@@ -171,15 +145,11 @@ namespace BizTalk.Adapter.Atom
                 {
                     this.feedMax = Int32.Parse(feedMax.InnerText);
                 }
-                
-                
-
             }
             catch (Exception ex)
             {
-                throw new ArgumentNullException("One or more configuration parameters are missing!",ex);
+                throw new ArgumentNullException("One or more configuration parameters are missing!", ex);
             }
-           
         }
 
         private string StringIFNull(XmlNode node)
@@ -187,9 +157,9 @@ namespace BizTalk.Adapter.Atom
             return StringIFNull(node, "");
         }
 
-        private string StringIFNull(XmlNode node,string replacement)
+        private string StringIFNull(XmlNode node, string replacement)
         {
-            if(node == null)
+            if (node == null)
             {
                 return replacement;
             }
