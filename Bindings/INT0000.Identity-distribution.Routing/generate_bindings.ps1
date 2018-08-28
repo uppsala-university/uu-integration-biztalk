ce28bb80a25e0d0a65fc0c46fd4b1b6896d3c78e
ministerkrister <kristofer.strom@uadm.uu.se>
2018-08-28 09:01:19 +0200

    Fix bindings 0000 group

---
+++ Bindings/INT0000.Identity-distribution.Routing/generate_bindings.ps1
@@ -0,0 +1,13 @@
+$DevBindings = 'INT0000.Identity-distribution.Routing.BindingInfo_Dev.xml'
+$TestBindings = 'INT0000.Identity-distribution.Routing.BindingInfo_Test.xml'
+$ProdBindings = 'INT0000.Identity-distribution.Routing.BindingInfo_Prod.xml'
+
+$DevQueue  = 'OS:.'
+$TestQueue  = 'OS:UUC-BIZSRV021-T'
+$ProdQueue  = 'OS:UUC-MSMQSRV002'
+
+copy .\$DevBindings .\$TestBindings
+(Get-Content -Path .\$TestBindings).Replace($DevQueue,$TestQueue) | Set-Content -Path .\$TestBindings
+
+copy .\$DevBindings .\$ProdBindings
+(Get-Content -Path .\$ProdBindings).Replace($DevQueue,$ProdQueue) | Set-Content -Path .\$ProdBindings
