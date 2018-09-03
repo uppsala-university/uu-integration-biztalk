e4699a72b9d605ada1f54077f16a6bbdb5e6a763
ministerkrister <kristofer.strom@uadm.uu.se>
2018-09-03 10:36:40 +0200

    Verifiering4 bindings

--- INT0016.Ladok3.Client/Src/Files/generate_bindings.ps1
+++ INT0016.Ladok3.Client/Src/Files/generate_bindings.ps1
@@ -1,13 +1,28 @@
 $DevBindings = 'INT0016.Ladok3.Client.BindingInfo_Dev.xml'
 $TestBindings = 'INT0016.Ladok3.Client.BindingInfo_Test.xml'
+$VerBindings = 'INT0016.Ladok3.Client.BindingInfo_Ver.xml'
 $ProdBindings = 'INT0016.Ladok3.Client.BindingInfo_Prod.xml'
 
 $DevLadok  = 'https://api.mit-integration.ladok.se/'
 $TestLadok  = 'https://api.mit-integration.ladok.se/'
+$VerLadok  = 'https://api.verifiering4.ladok.se/'
 $ProdLadok  = 'https://api.mit-integration.ladok.se/'
 
+$DevClientThumb  = '7B6E9150DD5B9EA485F23B1393398F583F53D90E'
+$TestClientThumb  = '7B6E9150DD5B9EA485F23B1393398F583F53D90E'
+$VerClientThumb  = ''
+$ProdClientThumb  = ''
+
+$DevServiceThumb  = '50DB3B6EB8AB6149A15F4EAFB9529995D45D2B1B'
+$TestServiceThumb  = '50DB3B6EB8AB6149A15F4EAFB9529995D45D2B1B'
+$VerServiceThumb  = ''
+$ProdServiceThumb  = ''
+
 copy .\$DevBindings .\$TestBindings
-(Get-Content -Path .\$TestBindings).Replace($DevLadok,$TestLadok) | Set-Content -Path .\$TestBindings
+(Get-Content -Path .\$TestBindings).Replace($DevLadok,$TestLadok).Replace($DevClientThumb,$TestClientThumb).Replace($DevServiceThumb,$TestServiceThumb) | Set-Content -Path .\$TestBindings
+
+copy .\$DevBindings .\$VerBindings
+(Get-Content -Path .\$VerBindings).Replace($DevLadok,$VerLadok).Replace($DevClientThumb,$VerClientThumb).Replace($DevServiceThumb,$VerServiceThumb) | Set-Content -Path .\$VerBindings
 
 copy .\$DevBindings .\$ProdBindings
-(Get-Content -Path .\$ProdBindings).Replace($DevLadok,$ProdLadok) | Set-Content -Path .\$ProdBindings
+(Get-Content -Path .\$ProdBindings).Replace($DevLadok,$ProdLadok).Replace($DevClientThumb,$ProdClientThumb).Replace($DevServiceThumb,$ProdServiceThumb)  | Set-Content -Path .\$ProdBindings
