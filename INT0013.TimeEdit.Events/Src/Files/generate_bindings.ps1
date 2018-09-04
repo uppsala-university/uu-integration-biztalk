500a0a967796a736f121d1bdf849922d7300e100
kimst498 <kimst498@UUC-BIZDEV008>
2018-09-04 18:00:09 +0200

    Fixing map-error (language)

--- INT0013.TimeEdit.Events/Src/Files/generate_bindings.ps1
+++ INT0013.TimeEdit.Events/Src/Files/generate_bindings.ps1
@@ -1,17 +1,28 @@
 $DevBindings = 'INT0013.TimeEdit.Events.BindingInfo_dev.xml'
 $TestBindings = 'INT0013.TimeEdit.Events.BindingInfo_Test.xml'
 $ProdBindings = 'INT0013.TimeEdit.Events.BindingInfo_Prod.xml'
+$VerBindings = 'INT0013.TimeEdit.Events.BindingInfo_Ver.xml'
 
 $DevQueue  = 'OS:.'
 $TestQueue  = 'OS:UUC-BIZSRV021-T'
 $ProdQueue  = 'OS:UUC-MSMQSRV002'
+$VerQueue  = 'OS:UUC-MSMQSRV001'
+
+$DevFolder  = 'C:\Integrations\INT0013.TimeEdit.Events\'
+$TesFolder  = '\\uuc-biz025-t.exchange-test.its.uu.se\timeedit$\'
+$ProdFolder  = '\\UUC-BIZ015.user.uu.se\timeedit$\'
+$VerFolder  = '\\uuc-biz008-t.exchange-test.its.uu.se\timeedit$\'
 
 $DevDB  = 'sftp://uuc-eko004.its.uu.se:22/timeedit/%MessageID%.xml'
 $TestDB  = 'sftp://uuc-eko004.its.uu.se:22/timeedit/%MessageID%.xml'
 $ProdDB  = 'sftp://uuc-eko004.its.uu.se:22/timeedit/%MessageID%.xml'
+$VerDB  = 'sftp://uuc-eko004.its.uu.se:22/timeedit/%MessageID%.xml'
 
 copy .\$DevBindings .\$TestBindings
-(Get-Content -Path .\$TestBindings).Replace($DevQueue,$TestQueue).Replace($DevDB,$TestDB) | Set-Content -Path .\$TestBindings
+(Get-Content -Path .\$TestBindings).Replace($DevQueue,$TestQueue).Replace($DevDB,$TestDB).Replace($DevFolder,$TesFolder)  | Set-Content -Path .\$TestBindings
 
 copy .\$DevBindings .\$ProdBindings
-(Get-Content -Path .\$ProdBindings).Replace($DevQueue,$ProdQueue).Replace($DevDB,$ProdDB) | Set-Content -Path .\$ProdBindings
+(Get-Content -Path .\$ProdBindings).Replace($DevQueue,$ProdQueue).Replace($DevDB,$ProdDB).Replace($DevFolder,$ProdFolder)  | Set-Content -Path .\$ProdBindings
+
+copy .\$DevBindings .\$VerBindings
+(Get-Content -Path .\$VerBindings).Replace($DevQueue,$VerQueue).Replace($DevDB,$VerDB).Replace($DevFolder,$VerFolder)  | Set-Content -Path .\$VerBindings
