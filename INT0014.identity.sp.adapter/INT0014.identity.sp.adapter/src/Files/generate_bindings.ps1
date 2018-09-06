3cd195581d9e8f3a6f76e47c2cd5dbdefb47c3b2
larvi530 <lars.vising@uadm.uu.se>
2018-09-06 09:39:31 +0200

    Added queue bindings

--- INT0014.identity.sp.adapter/INT0014.identity.sp.adapter/src/Files/generate_bindings.ps1
+++ INT0014.identity.sp.adapter/INT0014.identity.sp.adapter/src/Files/generate_bindings.ps1
@@ -2,6 +2,11 @@ $DevBindings = 'INT0014.identity.sp.adapter.bindings.DEV.xml'
 $TestBindings = 'INT0014.identity.sp.adapter.bindings.TEST.xml'
 $VerBindings = 'INT0014.identity.sp.adapter.bindings.VER.xml'
 $ProdBindings = 'INT0014.identity.sp.adapter.bindings.PROD.xml'
+
+$DevQueue  = 'OS:.'
+$TestQueue  = 'OS:UUC-BIZSRV021-T'
+$ProdQueue  = 'OS:UUC-MSMQSRV002'
+$VerQueue  = 'OS:UUC-MSMQSRV001'
  
 $DevSp  = 'https://spl3t.studentportalen.uu.se/uusp-webapp/spring/account/pnrupdate'
 $TestSp  = 'https://spl3t.studentportalen.uu.se/uusp-webapp/spring/account/pnrupdate'
@@ -10,9 +15,12 @@ $ProdSp  = 'https://spl3t.studentportalen.uu.se/uusp-webapp/spring/account/pnrup
 
 copy .\$DevBindings .\$TestBindings
 (Get-Content -Path .\$TestBindings).Replace($DevSp,$TestSp) | Set-Content -Path .\$TestBindings
+(Get-Content -Path .\$TestBindings).Replace($DevQueue,$TestQueue)| Set-Content -Path .\$TestBindings
 
 copy .\$DevBindings .\$VerBindings
 (Get-Content -Path .\$VerBindings).Replace($DevSp,$VerSp) | Set-Content -Path .\$VerBindings
+(Get-Content -Path .\$ProdBindings).Replace($DevQueue,$VerQueue) | Set-Content -Path .\$VerBindings
 
 copy .\$DevBindings .\$ProdBindings
 (Get-Content -Path .\$ProdBindings).Replace($DevSp,$ProdSp) | Set-Content -Path .\$ProdBindings
+(Get-Content -Path .\$ProdBindings).Replace($DevQueue,$ProdQueue) | Set-Content -Path .\$ProdBindings
