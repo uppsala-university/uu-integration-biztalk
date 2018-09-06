3a67e38515cfe53d3f25505fc967040a90fe67ef
larvi530 <lars.vising@uadm.uu.se>
2018-09-06 14:47:51 +0200

    added send-file bindings

--- INT0017.Usmr.Events/Src/Files/generate_bindings.ps1
+++ INT0017.Usmr.Events/Src/Files/generate_bindings.ps1
@@ -12,6 +12,11 @@ $Userreplace = 'REPLACEUSER'
 $TestDBuser  = 'sd-usmr-test-writer'
 $VerDBuser  =  'someuser'
 $ProdDBuser  = 'someuser'
+
+$DevFile  = 'C:\Integrations\INT0017.Usmr.Events'
+$TestFile  = '\\uuc-biz025-t.exchange-test.its.uu.se\usmr$'
+$ProdFile  = '\\UUC-BIZ015.user.uu.se\usmr$'
+$VerFile  = '\\uuc-biz008-t.exchange-test.its.uu.se\usmr$'
  
 $DevDB  = 'mssql://localhost:1433//sd-usmr-test-db'
 $TestDB  = 'mssql://SQL-AG1.exchange-test.its.uu.se:1433//sd-usmr-test-db'
@@ -22,13 +27,16 @@ copy .\$DevBindings .\$TestBindings
 (Get-Content -Path .\$TestBindings).Replace($DevDB,$TestDB) | Set-Content -Path .\$TestBindings
 (Get-Content -Path .\$TestBindings).Replace($DevQueue,$TestQueue)| Set-Content -Path .\$TestBindings
 (Get-Content -Path .\$TestBindings).Replace($Userreplace,$TestDBuser)| Set-Content -Path .\$TestBindings
+(Get-Content -Path .\$TestBindings).Replace($DevFile,$TestFile)| Set-Content -Path .\$TestBindings
 
 copy .\$DevBindings .\$VerBindings
 (Get-Content -Path .\$VerBindings).Replace($DevDB,$VerDB) | Set-Content -Path .\$VerBindings
 (Get-Content -Path .\$VerBindings).Replace($DevQueue,$VerQueue) | Set-Content -Path .\$VerBindings
 (Get-Content -Path .\$VerBindings).Replace($Userreplace,$VerDBuser) | Set-Content -Path .\$VerBindings
+(Get-Content -Path .\$VerBindings).Replace($Devfile,$VerFile) | Set-Content -Path .\$VerBindings
 
 copy .\$DevBindings .\$ProdBindings
 (Get-Content -Path .\$ProdBindings).Replace($DevDB,$ProdDB) | Set-Content -Path .\$ProdBindings
 (Get-Content -Path .\$ProdBindings).Replace($DevQueue,$ProdQueue) | Set-Content -Path .\$ProdBindings
 (Get-Content -Path .\$ProdBindings).Replace($Userreplace,$ProdDBuser) | Set-Content -Path .\$ProdBindings
+(Get-Content -Path .\$ProdBindings).Replace($DevFile,$ProdFile) | Set-Content -Path .\$ProdBindings
