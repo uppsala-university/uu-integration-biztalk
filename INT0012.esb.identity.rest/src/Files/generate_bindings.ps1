2780fcfff9da59b2507af86c7493b0640feda199
kimst498 <kimst498@UUC-BIZDEV008>
2018-09-27 12:58:48 +0200

    Adding logging in esb-identity

--- INT0012.esb.identity.rest/src/Files/generate_bindings.ps1
+++ INT0012.esb.identity.rest/src/Files/generate_bindings.ps1
@@ -8,11 +8,16 @@ $TestQueue  = 'OS:UUC-BIZSRV021-T'
 $VerQueue  =  'OS:UUC-MSMQSRV001'
 $ProdQueue  = 'OS:UUC-MSMQSRV002'
 
+$DevArchive  = 'C:\Integrations\Archive\esb-identity'
+$TestArchive  = '\\uuc-biz025-t.exchange-test.its.uu.se\esb-identity$\archive'
+$VerArchive  =  '\\uuc-biz008-t.exchange-test.its.uu.se\esb-identity$\archive'
+$ProdArchive  = '\\uuc-ladokfs001.its.uu.se\BizTalk-root$\esb-identity'
+
 copy .\$DevBindings .\$TestBindings
-(Get-Content -Path .\$TestBindings).Replace($DevQueue,$TestQueue) | Set-Content -Path .\$TestBindings
+(Get-Content -Path .\$TestBindings).Replace($DevQueue,$TestQueue).Replace($DevArchive,$TestArchive) | Set-Content -Path .\$TestBindings
 
 copy .\$DevBindings .\$VerBindings
-(Get-Content -Path .\$VerBindings).Replace($DevQueue,$VerQueue) | Set-Content -Path .\$VerBindings
+(Get-Content -Path .\$VerBindings).Replace($DevQueue,$VerQueue).Replace($DevArchive,$VerArchive) | Set-Content -Path .\$VerBindings
 
 copy .\$DevBindings .\$ProdBindings
-(Get-Content -Path .\$ProdBindings).Replace($DevQueue,$ProdQueue) | Set-Content -Path .\$ProdBindings
+(Get-Content -Path .\$ProdBindings).Replace($DevQueue,$ProdQueue).Replace($DevArchive,$ProdArchive) | Set-Content -Path .\$ProdBindings
