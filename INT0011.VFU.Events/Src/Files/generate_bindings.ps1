623a22082cf6f19830c8b5df72f37b729692f4ba
_peasb031 <_peasb031@UUC-BIZDEV007-T>
2018-10-04 16:02:48 +0200

    New orchestration ProcessUppehallBorttagetProgram

--- INT0011.VFU.Events/Src/Files/generate_bindings.ps1
+++ INT0011.VFU.Events/Src/Files/generate_bindings.ps1
@@ -14,12 +14,17 @@ $TestUsersLDAP  = 'LDAP://exchange-test.its.uu.se:389/OU=Users,OU=Student,OU=Akk
 $ProdUsersLDAP = 'LDAP://user.uu.se/OU=Users,OU=Student,OU=Akka,DC=user,DC=uu,DC=se'
 $VerUsersLDAP  = 'LDAP://exchange-test.its.uu.se:389/OU=Users,OU=Student,OU=Akka,DC=exchange-test,DC=its,DC=uu,DC=se'
 
+$DevRestApi = 'https://vfu-test.its.uu.se/vfutest'
+$TestRestApi = 'https://vfu-test.its.uu.se/vfutest'
+$ProdRestApi = 'https://vfu.its.uu.se/vfu'
+$VerRestApi =  'https://vfu-test.its.uu.se/vfutest'
+
 copy .\$DevBindings .\$TestBindings
-(Get-Content -Path .\$TestBindings).Replace($DevQueue,$TestQueue).Replace($DevUsersLDAP,$TestUsersLDAP) | Set-Content -Path .\$TestBindings
+(Get-Content -Path .\$TestBindings).Replace($DevQueue,$TestQueue).Replace($DevUsersLDAP,$TestUsersLDAP).Replace($DevRestApi,$TestRestApi) | Set-Content -Path .\$TestBindings
 
 copy .\$DevBindings .\$ProdBindings
-(Get-Content -Path .\$ProdBindings).Replace($DevQueue,$ProdQueue).Replace($DevUsersLDAP,$ProdUsersLDAP) | Set-Content -Path .\$ProdBindings
+(Get-Content -Path .\$ProdBindings).Replace($DevQueue,$ProdQueue).Replace($DevUsersLDAP,$ProdUsersLDAP).Replace($DevRestApi,$ProdRestApi)  | Set-Content -Path .\$ProdBindings
 
 copy .\$DevBindings .\$VerBindings
-(Get-Content -Path .\$VerBindings).Replace($DevQueue,$VerQueue).Replace($DevUsersLDAP,$VerUsersLDAP) | Set-Content -Path .\$VerBindings
+(Get-Content -Path .\$VerBindings).Replace($DevQueue,$VerQueue).Replace($DevUsersLDAP,$VerUsersLDAP).Replace($DevRestApi,$VerRestApi)  | Set-Content -Path .\$VerBindings
 
